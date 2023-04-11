cec63509fca0dd2d13c299a23417565dc9bc2681
Nadia Kurovska <nadiazabara@gmail.com>
2023-04-11 08:19:36 +0300

    add solution  from the  lesson

--- index.js
+++ index.js
@@ -3,19 +3,40 @@
 function totalWithDiscount (){
     let sum = +prompt('Type in the total sum of your purchase');
 if(sum >= 1500){
-    console.log(sum - ((sum / 100) * 10));
+    console.log(sum - ((sum / 100) * 10)); 
     } else if(sum >= 500){
-        console.log(sum - ((sum / 100) * 5));
+        console.log(sum - ((sum / 100) * 5));//sum * 0.95 // sum - (sum*0.05)
     } else{
         alert('keep on byuing!');
     }
 }
 totalWithDiscount();
 
+// const startingPrice = +prompt('');
+// let totalPrice;
+// if(startingPrice >= 500 && startingPrice < 1500) {
+//     totalPrice = sum * 0.95;
+// } else if (startingPrice >= 1500) {
+//     totalPrice = sum * 0.9;
+// } else {
+//     totalPrice = startingPrice;
+// }
+// alert('you will pay: '+ totalPrice);
+
+// if (startingPrice >= 1500){
+//     totalPrice = startingPrice - (startingPrice *0.1)
+// } else if (startingPrice >= 500){
+//     totalPrice = startingPrice - (startingPrice *0.05)    
+// } else if (starting price > 0){
+//     totalPrice = startingPrice;
+// } else{
+//     alert('incorrect value');
+// }
+
 
 
 //task 2
-function totalPrice(productSelected, productQuantity){
+const totalPrice = function(productSelected, productQuantity){
     let apples = 10;
     let oranges = 20;
     let coconuts = 25;
@@ -23,26 +44,55 @@ function totalPrice(productSelected, productQuantity){
 
     let productSelected = prompt('what would you like to buy?');
     let productQuantity = +prompt('how many items would you like?');
-    // let totalPrice = productSelected * productQuantity;
+    // let totalProductPrice = productSelected * productQuantity;
 
     switch (productSelected){ 
-        case 'apples':
+        case 'apples':{
             console.log(apples * productQuantity);
             break;
-            case 'oranges':
-                console.log(oranges * productQuantity);
-                break;
-                case 'coconuts':
-                console.log(coconuts * productQuantity);
-                break;
-                case 'watermelons':
-                console.log(watermelons * productQuantity);
-                break;
-                default:
-                    console.log('your order is not possible');
+        }
+        case 'oranges':{
+            console.log(oranges * productQuantity);
+            break;
+        }
+        case 'coconuts':{
+            console.log(coconuts * productQuantity);
+            break;
+        }
+        case 'watermelons':{
+            console.log(watermelons * productQuantity);
+            break;
+        }
+        default:{
+            console.log('your order is not possible');
+        }
     }
 }
 
 totalPrice();
 
-    
+// switch (productSelected){ 
+//     case 'apples':{
+//         if(productQuantity >= 0){
+//             console.log(apples * productQuantity);
+//         }else {
+//             console.log('bad quantity');
+//         }
+//         break;
+//     }
+//     case 'oranges':{
+//         console.log(oranges * productQuantity);
+//         break;
+//     }
+//     case 'coconuts':{
+//         console.log(coconuts * productQuantity);
+//         break;
+//     }
+//     case 'watermelons':{
+//         console.log(watermelons * productQuantity);
+//         break;
+//     }
+//     default:{
+//         console.log('your order is not possible');
+//     }
+// }
